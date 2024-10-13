7eefb33057f717314bbdfa58c960b2f65d8eb249
Z ❤'s Life <96372751+zcraftelite9495@users.noreply.github.com>
2024-10-13 13:12:58 -0400

    Update updatefromgh.lua

--- updatefromgh.lua
+++ updatefromgh.lua
@@ -1,3 +1,5 @@
 -- Use shell.run() to execute shell commands from Lua
 shell.run("delete", "time.lua")
 shell.run("wget", "https://raw.githubusercontent.com/zcraftelite9495/cctweakedstuff/refs/heads/main/time.lua", "time.lua")
+shell.run("delete", "updatefromgh.lua")
+shell.run("wget", "https://raw.githubusercontent.com/zcraftelite9495/cctweakedstuff/refs/heads/main/time.lua", "updatefromgh.lua")
