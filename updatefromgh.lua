bcdbf305002b46ab3941ba36847545efdc2c36f5
Z ❤'s Life <96372751+zcraftelite9495@users.noreply.github.com>
2024-10-13 13:14:10 -0400

    Update updatefromgh.lua

--- updatefromgh.lua
+++ updatefromgh.lua
@@ -2,4 +2,4 @@
 shell.run("delete", "time.lua")
 shell.run("wget", "https://raw.githubusercontent.com/zcraftelite9495/cctweakedstuff/refs/heads/main/time.lua", "time.lua")
 shell.run("delete", "updatefromgh.lua")
-shell.run("wget", "https://raw.githubusercontent.com/zcraftelite9495/cctweakedstuff/refs/heads/main/time.lua", "updatefromgh.lua")
+shell.run("wget", "https://raw.githubusercontent.com/zcraftelite9495/cctweakedstuff/refs/heads/main/updatefromgh.lua", "updatefromgh.lua")
